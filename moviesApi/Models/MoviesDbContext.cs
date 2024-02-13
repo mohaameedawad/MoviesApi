71ca7b74b296a8e39e2f9ae636461da01f7ef337
mohamed awaad <mohamed awaad@LAPTOP-QIUBQLSR>
2024-02-14 00:53:13 +0200

    Add project files.

---
+++ moviesApi/Models/MoviesDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace moviesApi.Models
+{
+    public class MoviesDbContext : DbContext 
+    {
+        public MoviesDbContext(DbContextOptions<MoviesDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
---
+++ moviesApi/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using moviesApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnenction");
+builder.Services.AddDbContext<MoviesDbContext>( options => options.UseSqlServer(connectionString));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddCors();
+
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors(c => c.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
